f1c494e1292236b5bc24856ae0b6ceedb59d5e62
406012+reetammitra2904@users.noreply.github.com <406012+reetammitra2904@users.noreply.github.com>
2021-01-31 21:07:12 -0500

    Correcting the download URL for JDK 8

--- multiple-java-versions.sh
+++ multiple-java-versions.sh
@@ -1,12 +1,12 @@
 
 #!/bin/bash
 #JDK LTS Download URLs (64 bit)
-# 8 ->  https://builds.openlogic.com/downloadJDK/openlogic-openjdk-jre/8u262-b10/openlogic-openjdk-jre-8u262-b10-linux-x64.tar.gz
+# 8 ->  https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u262-b10/openlogic-openjdk-8u262-b10-linux-x32.tar.gz
 # 11 -> https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_linux-x64_bin.tar.gzhttps://download.java.net/java/GA/jdk15/779bf45e88a44cbd9ea6621d33e33db1/36/GPL/openjdk-15_linux-x64_bin.tar.gz
 # 17 -> https://download.java.net/java/early_access/jdk17/7/GPL/openjdk-17-ea+7_linux-x64_bin.tar.gz
 
-JAVA_DOWNLOAD_URL=
-JAVA_VERSION=
+JAVA_DOWNLOAD_URL=https://builds.openlogic.com/downloadJDK/openlogic-openjdk/8u262-b10/openlogic-openjdk-8u262-b10-linux-x32.tar.gz
+JAVA_VERSION=8
 apt install wget
 filename=$(basename "$JAVA_DOWNLOAD_URL")
 wget "$JAVA_DOWNLOAD_URL"
